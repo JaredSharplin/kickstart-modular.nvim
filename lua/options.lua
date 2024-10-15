e17e9f5dc51a31f9ac7febca7cd7b6a1dd56ca6c
Jared Sharplin <jaredsharplin1@gmail.com>
2024-10-15 19:56:07 +1000

    Set options

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -51,15 +51,24 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '» ', trail = '', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
+
+-- Live show substitutions in the buffer
+vim.opt.incsearch = true
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Disable wrapping
+vim.opt.wrap = false
+
+-- Remove end of file ~
+vim.opt.fillchars:append ',eob: '
 
 -- vim: ts=2 sts=2 sw=2 et
