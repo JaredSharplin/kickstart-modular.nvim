343ff2436366d657a3b2ed65bbf6e4caf40a4a1e
Jared Sharplin <jaredsharplin1@gmail.com>
2024-10-15 19:51:41 +1000

    Add hidesig and endwise

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -15,7 +15,13 @@ return {
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
+      hidesig = {
+        enable = true,
+        opacity = 0.5, -- opacity for sig definitions
+        delay = 200, -- update delay on CursorMoved and InsertLeave
+      },
       indent = { enable = true, disable = { 'ruby' } },
+      endwise = { enable = true },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
