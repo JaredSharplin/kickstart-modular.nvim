f2a6433524656106e9f596bd01bfc451fa2864b2
Jared Sharplin <jaredsharplin1@gmail.com>
2025-01-06 23:11:07 +1000

    Disable additional_vim_regex_highlighting

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -23,7 +23,9 @@ return {
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+
+        --  NOTE:Don't enable this! Causes slowdown for ruby files
+        -- additional_vim_regex_highlighting = { 'ruby' },
       },
       hidesig = {
         enable = true,
