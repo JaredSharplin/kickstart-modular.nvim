5958a1a2c4b70f56d9c51343142b761438a20310
Jared Sharplin <jaredsharplin1@gmail.com>
2025-01-06 23:08:57 +1000

    Disable gitsigns

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -23,7 +23,8 @@ require('lazy').setup({
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
+  -- NOTE: Don't enable this, it causes massive slowdown with git commands enumerating untracked files
+  -- require 'kickstart/plugins/gitsigns',
 
   require 'kickstart/plugins/which-key',
 
