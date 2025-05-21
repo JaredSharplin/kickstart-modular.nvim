66a6c0ddd3f5b9d879dda39cfc1faea9a3e2b959
Jared Sharplin <jaredsharplin1@gmail.com>
2025-05-21 13:46:10 +1000

    Use anthropic in code companion

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -3,12 +3,19 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
-    'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
-    'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
-    { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
   },
   config = function()
     require('codecompanion').setup {}
+    require('codecompanion').setup {
+      strategies = {
+        chat = {
+          adapter = 'anthropic',
+        },
+        inline = {
+          adapter = 'anthropic',
+        },
+      },
+    }
 
     vim.api.nvim_set_keymap('n', '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('v', '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
